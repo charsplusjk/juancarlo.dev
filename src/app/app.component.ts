162f705fb384a483a44b59460617e0e1012c207f
juan <142954292+charsplusjk@users.noreply.github.com>
2023-09-30 18:11:45 -0300

    add: typed.js for typewriter effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit{
   title = 'juancarlo.dev';
+
+  constructor() {}
+
+  ngOnInit() {
+    const options = {
+     strings: ['carente em busac e sexo', 'calvo', 'testanto coisas'],
+     typeSpeed: 25,
+     backSpeed: 25,
+     showCursor: true,
+     cursorChar: '|',
+     loop: true
+    };
+
+    const typed = new Typed('.typed-element', options);
+  }
 }

b9c993a804b96114c83acd3a2cbe28b200a03b2d
juan <142954292+charsplusjk@users.noreply.github.com>
2024-12-05 11:05:49 -0300

    feat(*): create index page and integrate with chakra ui

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
+const Page = () => {
+    return (
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello I&apos;m a full stack developer based in Brazil!
+            </Box>
+            <Box display={{md:'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Juan Carlo
+                    </Heading>
+                    <p>Tech enthusiast</p>
+                </Box>
+            </Box>
+        </Container>
+    )
+}
+
+export default Page

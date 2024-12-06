a40ba8e7769a2740b258a2d1e5cbcd8c6a39505f
juan <142954292+charsplusjk@users.noreply.github.com>
2024-12-06 10:55:32 -0300

    feat(index.js): add profile picture

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
                 Hello I&apos;m a full stack developer based in Brazil!
             </Box>
             <Box display={{md:'flex'}}>
@@ -12,6 +12,9 @@ const Page = () => {
                         Juan Carlo
                     </Heading>
                     <p>Tech enthusiast</p>
+                </Box>
+                <Box flexShrink={0} mt={{base: 4, md:0}} ml={{md: 6}} align="center">
+                    <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" display="inline-block" borderRadius="full" alt="Profile Image" src="/images/green.jpg" />
                 </Box>
             </Box>
         </Container>

ea4e2f5415a784eb0770990a28423452017bbd68
juan <142954292+charsplusjk@users.noreply.github.com>
2024-12-12 18:11:03 -0300

    feat(main.js): add three.js car model

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import { Box, Container } from '@chakra-ui/react'
 import Navbar from '../navbar'
+import VoxelCar from '../voxel-car'
+import NoSsr from '../no-ssr'
 
 const Main = ({ children, router }) => {
     return (
@@ -13,6 +15,9 @@ const Main = ({ children, router }) => {
             <Navbar path={router.asPath} />
 
             <Container maxW="container.md" pt={14}>
+                <NoSsr>
+                    <VoxelCar />
+                </NoSsr>
                 {children}
             </Container>
         </Box>
